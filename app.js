8881c93307af63b005f0da69d85ee5709f1b501c
D3M6 <demg.working@gmail.com>
2021-05-02 13:28:51 +0200

    Updates

---
+++ app.js
@@ -0,0 +1,53 @@
+//
+// Window load event
+//
+window.addEventListener('DOMContentLoaded', () => {
+    //
+    // Global variables
+    //
+    let isNavMobileOpen = false;
+    //
+    // HTML elements
+    //
+    const navbarMobile = document.querySelector('.nav-mobile');
+    const footer = document.querySelector('.footer');
+    //
+    // Function to check ScrollPosition
+    //
+    const checkScrollPosition = (scroll) => {
+        if (isNavMobileOpen) {
+            if (scroll < 300) {
+            navbarMobile.classList.remove('d-flex');
+            navbarMobile.classList.add('d-none');
+            } else if (scroll >= 300 && scroll < (footer.offsetTop - 1000)) {
+            navbarMobile.classList.remove('d-none');
+            navbarMobile.classList.add('d-flex');
+            } else if (scroll > (footer.offsetTop - 1000)) {
+            navbarMobile.classList.remove('d-flex');
+            navbarMobile.classList.add('d-none');
+            }
+        }
+    };
+    //
+    // Window scroll event
+    //
+    window.addEventListener('scroll', () => {
+        if (window.innerWidth < 1200 && isNavMobileOpen) {
+            const scroll = window.pageYOffset;
+            checkScrollPosition(scroll);
+        }
+    });
+    //
+    // Window scroll event
+    //
+    window.addEventListener('resize', () => {
+        if (window.innerWidth < 1200) {
+            isNavMobileOpen = true;
+        } else {
+            isNavMobileOpen = false;
+            navbarMobile.classList.remove('d-flex');
+            navbarMobile.classList.add('d-none');
+        }
+    });
+});
+
